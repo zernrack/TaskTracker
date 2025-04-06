28a303f619cf86da4613c0b8218f438bb1029d51
Renz Rackhold <rackhold@gmail.com>
2025-04-06 12:35:35 +0800

    Init springboot proj

---
+++ src/main/java/com/renz/TaskTracker/TaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.renz.TaskTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renz/TaskTracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renz.TaskTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
